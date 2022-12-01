2402b1c9a315f8c5d6b0bac692e5ff24c726fd4a
HARVINDAR SINGH <W9355748@live.tees.ac.uk>
2022-12-01 16:23:10 +0000

    contact form

---
+++ SeerBI.Web/Controllers/Contacts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeerBI.Web.Controllers
+{
+    public class Contacts : Controller
+    {
+
+        // GET: Companies/Create
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Companies/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
