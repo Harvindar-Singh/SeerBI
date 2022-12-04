31dba4a587bd865cd6894395c21d4f5d6c295636
HARVINDAR SINGH <W9355748@live.tees.ac.uk>
2022-12-04 20:57:10 +0000

    news menu

--- SeerBI.Web/Controllers/News.cs
+++ SeerBI.Web/Controllers/News.cs
@@ -10,6 +10,57 @@ namespace SeerBI.Web.Controllers
         {
             return View();
         }
+        // GET: News1
+        public ActionResult News1()
+        {
+            return View();
+        }
+        // GET: News2
+        public ActionResult News2()
+        {
+            return View();
+        }
+        // GET: News3
+        public ActionResult News3()
+        {
+            return View();
+        }
+        // GET: News4
+        public ActionResult News4()
+        {
+            return View();
+        }
+        // GET: News5
+        public ActionResult News5()
+        {
+            return View();
+        }
+        // GET: News6
+        public ActionResult News6()
+        {
+            return View();
+        }
+        // GET: News7
+        public ActionResult News7()
+        {
+            return View();
+        }
+        // GET: News8
+        public ActionResult News8()
+        {
+            return View();
+        }
+        // GET: News9
+        public ActionResult News9()
+        {
+            return View();
+        }
+        // GET: News10
+        public ActionResult News10()
+        {
+            return View();
+
+        }
 
         // GET: News/Details/5
         public ActionResult Details(int id)
